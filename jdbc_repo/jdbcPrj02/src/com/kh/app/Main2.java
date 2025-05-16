7aa7afa77fe3f3c33b9fb29faa873e913506f495
florest <khflorest@gmail.com>
2025-05-16 17:38:33 +0900

    2nd commit
    .

--- jdbc_repo/jdbcPrj02/src/com/kh/app/Main2.java
+++ jdbc_repo/jdbcPrj02/src/com/kh/app/Main2.java
@@ -1,14 +1,16 @@
 package com.kh.app;
 
 import java.sql.Statement;
+import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class Main2 {
 
 	public static void main(String[] args) throws Exception {
 
-		System.out.println("Main2");
+		System.out.println("제이디비씨~~~");
 		
 		//driver
 		Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -19,11 +21,22 @@ public class Main2 {
 		String pwd = "1234";
 		Connection conn = DriverManager.getConnection(url, id, pwd);
 		
-		// SQL
-		String sql = "INSERT INTO STUDENT(NAME) VALUES('김철수')";
-		Statement stmt = conn.createStatement();
-		stmt.execute(sql);
+		//input
+		System.out.println("학생 이름 : ");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
 		
+		// SQL
+		String sql = "INSERT INTO STUDENT(NAME) VALUES(?)"; //입력받을 값 스캐너 // ?
+		PreparedStatement pstmt = conn.prepareStatement(sql); // 왜 처음부터 sql? // PreparedStatement는 부모..가 있어서? 바꿀 필요없다?
+		pstmt.setString(1, str);
+//		pstmt.setString('\n');
+//		pstmt.setString(2, "김태희");
+//		pstmt.setString(3, "김태희"); // 물음표 여러개라면
+		int result = pstmt.executeUpdate(); // stmt.쿼리실행 // sql 지움
+		System.out.println("실행결과 : " + result); // 1개 행이 insert 되었습니다
+		
+		// prepareStatement != executeUpdate ?
 	}//main
 
 }//class
